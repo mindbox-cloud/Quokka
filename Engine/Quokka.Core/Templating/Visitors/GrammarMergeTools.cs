d5c0447eeed12ab59783a67db26d46616a19b0df
Roman Ivonin <ivonin@mindbox.ru>
2015-10-27 13:33:18 +0300

    Fixed and improved merging of two grammars and reference discovery

---
+++ Engine/Quokka.Core/Templating/Visitors/GrammarMergeTools.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quokka
+{
+	internal class GrammarMergeTools
+	{
+		internal static List<IStaticBlockPart> MergeInnerAndOuterBlocks(
+			List<IStaticBlockPart> outerGrammarBlocks,
+			List<IStaticBlockPart> innerGrammarBlocks)
+		{
+			var result = new List<IStaticBlockPart>();
+
+			var currentInnerBlockIndex = 0;
+			var currentInnerBlock = innerGrammarBlocks.Count > 0
+				? innerGrammarBlocks[0]
+				: null;
+
+			foreach (var outerBlock in outerGrammarBlocks)
+			{
+				int outerBlockEnd = outerBlock.Offset + outerBlock.Length - 1;
+				int outerBlockPosition = outerBlock.Offset;
+
+				while (outerBlockPosition <= outerBlockEnd)
+				{
+					if (currentInnerBlock == null || currentInnerBlock.Offset > outerBlockEnd)
+					{
+						if (outerBlockPosition == outerBlock.Offset)
+						{
+							result.Add(outerBlock);
+						}
+						else
+						{
+							var constantOuterBlock = outerBlock as ConstantBlock;
+							if (constantOuterBlock == null)
+								throw new InvalidOperationException("Trying to split a non-constant block");
+
+							var triviaLength = outerBlockEnd - outerBlockPosition + 1;
+							result.Add(new ConstantBlock(
+								constantOuterBlock.Text.Substring(outerBlockPosition - outerBlock.Offset),
+								outerBlockPosition,
+								triviaLength));
+						}
+
+						outerBlockPosition = outerBlockEnd + 1;
+					}
+					else
+					{
+						int currentInnerBlockEnd = currentInnerBlock.Offset + currentInnerBlock.Length - 1;
+
+						if (currentInnerBlock.Offset >= outerBlockPosition)
+						{
+							if (currentInnerBlock.Offset > outerBlockPosition)
+							{
+								var constantOuterBlock = outerBlock as ConstantBlock;
+								if (constantOuterBlock == null)
+									throw new InvalidOperationException("Inner grammar block starts in a non-constant block");
+
+								var triviaLength = currentInnerBlock.Offset - outerBlockPosition;
+								var leadingTriviaBlock = new ConstantBlock(
+									constantOuterBlock.Text.Substring(outerBlockPosition, triviaLength),
+									outerBlockPosition,
+									triviaLength);
+								result.Add(leadingTriviaBlock);
+							}
+							result.Add(currentInnerBlock);
+						}
+
+						if (currentInnerBlockEnd <= outerBlockEnd)
+						{
+							outerBlockPosition = currentInnerBlockEnd + 1;
+
+							var nextInnerBlockIndex = currentInnerBlockIndex + 1;
+							var nextInnerBlock = innerGrammarBlocks.Count > nextInnerBlockIndex
+								? innerGrammarBlocks[nextInnerBlockIndex]
+								: null;
+
+							if (currentInnerBlockEnd < outerBlockEnd)
+							{
+								var constantOuterBlock = outerBlock as ConstantBlock;
+								if (constantOuterBlock == null)
+									throw new InvalidOperationException("Inner grammar block ends in a non-constant block");
+
+								var triviaLength = nextInnerBlock == null || nextInnerBlock.Offset > outerBlockEnd
+									? outerBlock.Length - outerBlockPosition + outerBlock.Offset
+									: nextInnerBlock.Offset - outerBlockPosition;
+
+								var trailingTriviaBlock = new ConstantBlock(
+									constantOuterBlock.Text.Substring(outerBlockPosition - outerBlock.Offset, triviaLength),
+									outerBlockPosition,
+									triviaLength);
+								result.Add(trailingTriviaBlock);
+
+								outerBlockPosition += triviaLength;
+							}
+
+							currentInnerBlockIndex = nextInnerBlockIndex;
+							currentInnerBlock = nextInnerBlock;
+						}
+						else
+						{
+							outerBlockPosition = outerBlockEnd + 1;
+						}
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Engine/Quokka.Tests/Html/GrammarMergingTests.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Quokka.Tests.Html
+{
+	[TestClass]
+	public class GrammarMergingTests
+	{
+		[TestMethod]
+		public void HtmlMerge_SingleInnerBlockSpansMultipleOuter()
+		{
+			var outer = new List<IStaticBlockPart>
+			{
+				new ConstantBlock("abcdeABCDEabcdeABCDE", 0, 20),
+				new ConstantBlock("${ param }", 20, 10),
+				new ConstantBlock("0123456789A", 30, 11)
+			};
+
+			var inner = new List<IStaticBlockPart>
+			{
+				new ConstantBlock(
+					"ZzZzYyYyXxXxYyYyZzZz",
+					14,
+					20)
+			};
+
+			var result = GrammarMergeTools.MergeInnerAndOuterBlocks(
+				outer,
+				inner)
+				.Cast<ConstantBlock>()
+				.ToList();
+
+			Assert.AreEqual("abcdeABCDEabcd", result[0].Text);
+			Assert.AreEqual("ZzZzYyYyXxXxYyYyZzZz", result[1].Text);
+			Assert.AreEqual("456789A", result[2].Text);
+		}
+
+		[TestMethod]
+		public void HtmlMerge_SingleInnerBlockExactlyMatchesOuter()
+		{
+			var outer = new List<IStaticBlockPart>
+			{
+				new ConstantBlock("abcdeABCDEabcdeABCDE", 0, 20),
+				new ConstantBlock("${ param }", 20, 10),
+				new ConstantBlock("0123456789A", 30, 11)
+			};
+
+			var inner = new List<IStaticBlockPart>
+			{
+				new ConstantBlock(
+					"_!_!_!_!_!",
+					20,
+					10)
+			};
+
+			var result = GrammarMergeTools.MergeInnerAndOuterBlocks(
+				outer,
+				inner)
+				.Cast<ConstantBlock>()
+				.ToList();
+
+			Assert.AreEqual("abcdeABCDEabcdeABCDE", result[0].Text);
+			Assert.AreEqual("_!_!_!_!_!", result[1].Text);
+			Assert.AreEqual("0123456789A", result[2].Text);
+		}
+
+		[TestMethod]
+		public void HtmlMerge_SingleInnerBlockInsideOuter()
+		{
+			var outer = new List<IStaticBlockPart>
+			{
+				new ConstantBlock("abcdeABCDEabcdeABCDE", 0, 20),
+				new ConstantBlock("${ param }", 20, 10),
+				new ConstantBlock("0123456789A", 30, 11)
+			};
+
+			var inner = new List<IStaticBlockPart>
+			{
+				new ConstantBlock(
+					"_!_!_!",
+					11,
+					6)
+			};
+
+			var result = GrammarMergeTools.MergeInnerAndOuterBlocks(
+				outer,
+				inner)
+				.Cast<ConstantBlock>()
+				.ToList();
+
+			Assert.AreEqual("abcdeABCDEa", result[0].Text);
+			Assert.AreEqual("_!_!_!", result[1].Text);
+			Assert.AreEqual("CDE", result[2].Text);
+			Assert.AreEqual("${ param }", result[3].Text);
+			Assert.AreEqual("0123456789A", result[4].Text);
+		}
+
+		[TestMethod]
+		public void HtmlMerge_MultipleInnerBlocksInsideSingleOuter()
+		{
+			var outer = new List<IStaticBlockPart>
+			{
+				new ConstantBlock("abcdeABCDEabcdeABCDE", 0, 20),
+				new ConstantBlock("${ param }", 20, 10),
+				new ConstantBlock("0123456789A", 30, 11)
+			};
+
+			var inner = new List<IStaticBlockPart>
+			{
+				new ConstantBlock(
+					"ZZZZZ",
+					5,
+					5),
+				new ConstantBlock(
+					"YYYYY",
+					15,
+					5)
+			};
+
+			var result = GrammarMergeTools.MergeInnerAndOuterBlocks(
+				outer,
+				inner)
+				.Cast<ConstantBlock>()
+				.ToList();
+
+			Assert.AreEqual("abcde", result[0].Text);
+			Assert.AreEqual("ZZZZZ", result[1].Text);
+			Assert.AreEqual("abcde", result[2].Text);
+			Assert.AreEqual("YYYYY", result[3].Text);
+			Assert.AreEqual("${ param }", result[4].Text);
+			Assert.AreEqual("0123456789A", result[5].Text);
+		}
+	}
+}
