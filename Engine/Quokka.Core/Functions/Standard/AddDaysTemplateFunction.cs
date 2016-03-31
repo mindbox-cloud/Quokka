53f2b62add9e98f0a2142a59870cecb2829ab3b4
Roman Ivonin <ivonin@mindbox.ru>
2016-03-31 03:06:31 +0300

    Now definitely added AddDays

---
+++ Engine/Quokka.Core/Functions/Standard/AddDaysTemplateFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Quokka
+{
+	internal class AddDaysTemplateFunction : ScalarTemplateFunction<DateTime, decimal, DateTime>
+	{
+		public AddDaysTemplateFunction()
+			: base(
+				  "addDays",
+				  new DateTimeFunctionArgument("date"), 
+				  new DecimalFunctionArgument("daysAmount"))
+		{
+		}
+
+		public override DateTime Invoke(DateTime date, decimal daysAmount)
+		{
+			return date.AddDays((double)daysAmount);
+		}
+	}
+}
