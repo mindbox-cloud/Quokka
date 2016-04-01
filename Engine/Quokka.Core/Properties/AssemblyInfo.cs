c696f600d70476c60e7c847db128bf6afa0618e4
Roman Ivonin <ivonin@mindbox.ru>
2016-04-02 01:18:24 +0300

    Prepared .nuspec file and AssemblyInfo for NuGet publishing

--- Engine/Quokka.Core/Properties/AssemblyInfo.cs
+++ Engine/Quokka.Core/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Quokka.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Quokka")]
+[assembly: AssemblyDescription("String templating library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Quokka.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Mindbox")]
+[assembly: AssemblyProduct("Quokka Template Engine")]
+[assembly: AssemblyCopyright("Copyright © Mindbox 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 
 [assembly: InternalsVisibleTo("Quokka.Tests")]
 [assembly: InternalsVisibleTo("Quokka.Sandbox")]
