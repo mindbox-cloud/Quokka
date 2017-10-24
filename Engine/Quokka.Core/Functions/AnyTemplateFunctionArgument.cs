7613db8bf43ea9ea7f2507fcff5bbffc08d88aca
Rostislav Listerenko <listerenko@mindbox.ru>
2017-10-24 15:41:22 +0300

    bugzid: 184111 Allowed isEmpty to be used with any argument type

---
+++ Engine/Quokka.Core/Functions/AnyTemplateFunctionArgument.cs
@@ -0,0 +1,16 @@
+﻿namespace Mindbox.Quokka
+{
+	internal class AnyTemplateFunctionArgument : TemplateFunctionArgument
+	{
+		public AnyTemplateFunctionArgument(string name) : base(name, true)
+		{
+		}
+
+		internal override TypeDefinition Type => TypeDefinition.Unknown;
+
+		internal override ArgumentValueValidationResult ValidateConstantValue(VariableValueStorage value)
+		{
+			return ArgumentValueValidationResult.Valid;
+		}
+	}
+}
--- Engine/Quokka.Core/Functions/Standard/IsEmptyTemplateFunction.cs
+++ Engine/Quokka.Core/Functions/Standard/IsEmptyTemplateFunction.cs
@@ -1,17 +1,36 @@
-﻿namespace Mindbox.Quokka
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mindbox.Quokka
 {
-	internal class IsEmptyTemplateFunction : ScalarTemplateFunction<string, bool>
+	internal class IsEmptyTemplateFunction : TemplateFunction
 	{
 		public IsEmptyTemplateFunction()
-			: base(
-				  "isEmpty",
-				  new StringFunctionArgument("string", allowsNull: true))
+			: base("isEmpty",
+					new PrimitiveModelDefinition(TypeDefinition.Boolean),
+					new AnyTemplateFunctionArgument("value"))
 		{
 		}
-
-		public override bool Invoke(string argument)
+		
+		internal override VariableValueStorage Invoke(IList<VariableValueStorage> argumentsValues)
 		{
-			return string.IsNullOrWhiteSpace(argument);
+			if (argumentsValues.Count != 1)
+				throw new InvalidOperationException($"Function that expects 1 argument was passed {argumentsValues.Count}");
+
+			var isEmpty = false;
+			var storage = argumentsValues.Single();
+			if (storage.CheckIfValueIsNull())
+			{
+				isEmpty = true;
+			}
+			else if (storage is PrimitiveVariableValueStorage primitiveVariableValueStorage)
+			{
+				var value = primitiveVariableValueStorage.GetPrimitiveValue();
+				if (value is string stringValue)
+					isEmpty = string.IsNullOrWhiteSpace(stringValue);
+			}
+			return new PrimitiveVariableValueStorage(isEmpty);
 		}
 	}
 }
