6b87c8167d031888c81022f03b5eefd1235a5782
Rostislav Listerenko <listerenko@mindbox.ru>
2018-09-03 13:45:37 +0300

    bugzid: 289735

---
+++ Engine/Quokka.Core/Functions/Standard/GetDayTemplateFunction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Mindbox.Quokka
+{
+    internal class GetDayTemplateFunction : ScalarTemplateFunction<DateTime, int>
+    {
+        public GetDayTemplateFunction()
+            : base(
+                "getDay",
+                new DateTimeFunctionArgument("dateTime"))
+        {
+        }
+
+        public override int Invoke(DateTime dateTime)
+        {
+            return dateTime.Day;
+        }
+    }
+}
