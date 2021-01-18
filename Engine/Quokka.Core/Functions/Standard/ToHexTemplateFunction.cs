f209e1f3c46d3583fd868861a8eb65d93ffa2d3e
Artem Sakhatskiy <sakhatskiy@yahoo.com>
2021-01-18 19:04:15 +0100

    Add toHex string function

---
+++ Engine/Quokka.Core/Functions/Standard/ToHexTemplateFunction.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace Mindbox.Quokka
+{
+	internal class ToHexTemplateFunction : ScalarTemplateFunction<string, string>
+	{
+		public ToHexTemplateFunction()
+			: base(
+				  "toHexadecimal",
+				  new StringFunctionArgument("string"))
+		{
+		}
+
+		public override string Invoke(string argument1)
+		{
+			return ByteUtility.ToHexString(Encoding.UTF8.GetBytes(argument1));
+		}
+	}
+}
---
+++ Engine/Quokka.Tests/Functions/ToHexTemplateFunctionTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mindbox.Quokka.Tests
+{
+	[TestClass]
+	public class ToHexTemplateFunctionTests
+	{
+		[TestMethod]
+		public void Invoke_ReturnsValidHexValue()
+		{
+			var email = "https://strange@email.com?some=parame&other#things";
+			var hexValue = new ToHexTemplateFunction();
+
+			var hash = hexValue.Invoke(email);
+			Assert.AreEqual(
+				"68747470733A2F2F737472616E676540656D61696C2E636F6D3F736F6D653D706172616D65266F74686572237468696E6773",
+				hash);
+		}
+	}
+}
