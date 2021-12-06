e1ed1754e1616c0e4bb78b2cd8917fcae0fc1d8d
Nikolay Andreychuk <andreychuk@mindbox.ru>
2021-12-06 18:29:48 +0300

    Добавил фнукцию хэширования по алгоритму SHA1

---
+++ Engine/Quokka.Tests/Functions/Sha1HashFunctionTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mindbox.Quokka.Tests
+{
+	[TestClass]
+	public class Sha1HashFunctionTests
+	{
+		[TestMethod]
+		public void Invoke_ReturnsValidHash()
+		{
+			var email = "strange@email.com";
+			var sha1 = new Sha1HashFunction();
+
+			var hash = sha1.Invoke(email);
+			Assert.AreEqual("4AD63D3592358CE97B2FC7F6C570C73944B278B5", hash);
+		}
+	}
+}
