99b8807563315b1da88c13739422004a79da5b43
Daniil Chervyakov <70147266+daniilchervyakov@users.noreply.github.com>
2022-05-16 18:04:52 +0300

    Move some functions from framework (#55)
    * Move some function from framework
    
    * Bump version
    
    Co-authored-by: Червяков Даниил <chervyakov@mindbox.ru>

---
+++ Engine/Quokka.Core/Functions/Standard/CapitalizeTemplateFunction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Mindbox.Quokka
+{
+	internal class CapitalizeTemplateFunction : ScalarTemplateFunction<string, string>
+	{
+		public CapitalizeTemplateFunction()
+			: base("capitalize", new StringFunctionArgument("value"))
+		{
+		}
+
+		public override string Invoke(string value)
+		{
+			if (value == null)
+				throw new ArgumentNullException(nameof(value));
+
+			var parts = value.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+				.Select((str, i) => i == 0 ? Capitalize(str) : str);
+
+			return string.Join(" ", parts);
+		}
+		
+		private static string Capitalize(string value)
+		{
+			if (value == null)
+				throw new ArgumentNullException(nameof(value));
+
+			if (value == string.Empty)
+				return value;
+
+			return value.Substring(0, 1).ToUpper() + value.Substring(1);
+		}
+	}
+}
---
+++ Engine/Quokka.Core/Functions/Standard/FormsTemplateFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Mindbox.Quokka
+{
+	internal class FormsTemplateFunction : ScalarTemplateFunction<decimal, string, string, string, string>
+	{
+		public FormsTemplateFunction()
+			: base(
+				"forms",
+				new DecimalFunctionArgument("quantity"),
+				new StringFunctionArgument("singularForm"),
+				new StringFunctionArgument("dualForm"),
+				new StringFunctionArgument("pluralForm"))
+		{
+		}
+
+		public override string Invoke(decimal quantity, string singularForm, string dualForm, string pluralForm)
+		{
+			var intQuantity = Convert.ToInt32(quantity);
+			return LanguageUtility.GetQuantityForm(intQuantity, singularForm, dualForm, pluralForm);
+		}
+	}
+}
