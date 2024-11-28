ab7f87636671a05e754d2750c2e3da84bf449fe3
Andrew Gronin <andrygronin3@gmail.com>
2024-11-28 14:41:34 +0300

    Grammar merge tools consider scenario which happens with zero length blocks

--- Engine/Quokka.Core/Templating/Visitors/GrammarMergeTools.cs
+++ Engine/Quokka.Core/Templating/Visitors/GrammarMergeTools.cs
@@ -88,43 +88,59 @@ namespace Mindbox.Quokka
 							result.Add(currentInnerBlock);
 						}
 
-						// Current inner grammar block is fully contained within current outer grammar block.
-						if (currentInnerBlockEnd <= outerBlockEnd)
+						// Current inner grammar block is zero length and precedes outer grammar block
+						if (currentInnerBlockEnd + 1 == outerBlockPosition)
 						{
-							outerBlockPosition = currentInnerBlockEnd + 1;
-
 							var nextInnerBlockIndex = currentInnerBlockIndex + 1;
 							var nextInnerBlock = innerGrammarBlocks.Count > nextInnerBlockIndex
-								? innerGrammarBlocks[nextInnerBlockIndex]
-								: null;
-
-							// There is a constant portion of outer grammar block that immediately follows the inner grammar block.
-							// This portion must be added to the result stream.
-							if (currentInnerBlockEnd < outerBlockEnd)
-							{
-								var constantOuterBlock = outerBlock as ConstantBlock;
-								if (constantOuterBlock == null)
-									throw new InvalidOperationException("Inner grammar block ends in a non-constant block");
-
-								var triviaLength = nextInnerBlock == null || nextInnerBlock.Offset > outerBlockEnd
-									? outerBlock.Length - outerBlockPosition + outerBlock.Offset
-									: nextInnerBlock.Offset - outerBlockPosition;
-
-								var trailingTriviaBlock = new ConstantBlock(
-									GetSubstringOfCodePoints(constantOuterBlock.Text, outerBlockPosition - outerBlock.Offset, triviaLength),
-									outerBlockPosition,
-									triviaLength);
-								result.Add(trailingTriviaBlock);
-
-								outerBlockPosition += triviaLength;
-							}
-
+													? innerGrammarBlocks[nextInnerBlockIndex]
+													: null;
 							currentInnerBlockIndex = nextInnerBlockIndex;
 							currentInnerBlock = nextInnerBlock;
 						}
 						else
 						{
-							outerBlockPosition = outerBlockEnd + 1;
+							// Current inner grammar block is fully contained within current outer grammar block.
+							if (currentInnerBlockEnd <= outerBlockEnd)
+							{
+								outerBlockPosition = currentInnerBlockEnd + 1;
+
+								var nextInnerBlockIndex = currentInnerBlockIndex + 1;
+								var nextInnerBlock = innerGrammarBlocks.Count > nextInnerBlockIndex //start
+														? innerGrammarBlocks[nextInnerBlockIndex]
+														: null;
+
+								// There is a constant portion of outer grammar block that immediately follows the inner grammar block.
+								// This portion must be added to the result stream.
+								if (currentInnerBlockEnd < outerBlockEnd)
+								{
+									var constantOuterBlock = outerBlock as ConstantBlock;
+									if (constantOuterBlock == null)
+										throw new InvalidOperationException("Inner grammar block ends in a non-constant block");
+
+									var triviaLength = nextInnerBlock == null || nextInnerBlock.Offset > outerBlockEnd
+															? outerBlock.Length - outerBlockPosition + outerBlock.Offset
+															: nextInnerBlock.Offset - outerBlockPosition;
+
+									var trailingTriviaBlock = new ConstantBlock(
+										GetSubstringOfCodePoints(
+											constantOuterBlock.Text,
+											outerBlockPosition - outerBlock.Offset,
+											triviaLength),
+										outerBlockPosition,
+										triviaLength);
+									result.Add(trailingTriviaBlock);
+
+									outerBlockPosition += triviaLength;
+								}
+
+								currentInnerBlockIndex = nextInnerBlockIndex;
+								currentInnerBlock = nextInnerBlock;
+							}
+							else
+							{
+								outerBlockPosition = outerBlockEnd + 1;
+							}
 						}
 					}
 				}
